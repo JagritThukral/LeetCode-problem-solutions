f99d124825177764da7a8069064da07c68215a03
Jagrit Thukral <jagritjtn@gmail.com>
2026-07-08 14:35:27 +0530

    Time: 65 ms (92.36%), Space: 98.8 MB (34.56%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <unordered_map>
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.contains(nums[i])) {
+                cout << "i " << map[nums[i]] << " j " << i << endl;
+                if (abs(map[nums[i]] - i) <= k)
+                    return true;
+            }
+            map[nums[i]] = i;
+        }
+        return false;
+    }
+};
